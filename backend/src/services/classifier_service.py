1a6115af8ab61485e5ae2738a7fcca6bfda2c1f9
Guilherme Luan <guilhermeluan@pm.me>
2025-09-27 14:26:19 -0300

    feat(api): implement email classification service
    * Add classify_email function to classify emails as Productive or Unproductive.
    * Introduce get_prompt function to load classification prompt from file.
    * Create email_classification.txt for prompt definitions and instructions.

--- backend/src/services/classifier_service.py
+++ backend/src/services/classifier_service.py
@@ -1,9 +1,28 @@
+from pathlib import Path
+
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
 load_dotenv(".venv")
 
-def classify_email(data: dict) -> dict:
 
-    return {'message': "productive", 'input': data}
+def classify_email(data: dict) -> dict:
+    genai.configure(api_key=os.getenv("GEMINI_API_TOKEN"))
+    model = genai.GenerativeModel("gemini-2.5-flash")
+
+    email_to_classify = data.get('email', '')
+
+    prompt = get_prompt().format(email_to_classify=email_to_classify)
+
+    response = model.generate_content(prompt)
+    return {'message': response.text}
+
+
+def get_prompt() -> str:
+    prompt_path = Path(__file__).parent.parent / "prompts" / "email_classification.txt"
+
+    with open(prompt_path, "r", encoding="utf-8") as f:
+        prompt_template = f.read()
+
+    return prompt_template
