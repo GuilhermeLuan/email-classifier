419a7df71fe21dfeadd4df7ac5ce04b57acb6e40
Guilherme Luan <guilhermeluan@pm.me>
2025-09-29 15:20:20 -0300

    feat(api): add email classification and reply generation functions
    * Implement classifyEmail function to classify emails via API.
    * Implement generateReply function to generate replies using API.

--- src/static/services/emailService.js
+++ src/static/services/emailService.js
@@ -1,1 +1,9 @@
 import api from "./api";
+
+export const classifyEmail = (emailData) => {
+    return api.post("/classify", emailData);
+}
+
+export const generateReply = (emailData) => {
+    return api.post("/generate-reply", emailData);
+}
