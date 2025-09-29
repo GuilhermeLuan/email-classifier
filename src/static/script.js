55dfb3c499796f8273fc4092ad28a4a62b5f25cb
Guilherme Luan <guilhermeluan@pm.me>
2025-09-29 16:56:38 -0300

    feat(ui): enhance email classification interface
    * Added alert container for user feedback on email submission
    * Implemented validation to ensure email content is provided before submission
    * Adjusted footer positioning for better layout

--- src/static/script.js
+++ src/static/script.js
@@ -6,7 +6,13 @@ const classifyButton = document.getElementById('classifyBtn');
 classifyButton.addEventListener('click', async () => {
     const emailContent = textArea.value.trim();
 
+    if (!emailContent) {
+        showBootstrapAlert("Digite um texto antes de enviar!");
+        return;
+    }
+
     try {
+
         const payload = {email: emailContent};
 
         const classifyResponse = await classifyEmail(payload);
@@ -16,3 +22,24 @@ classifyButton.addEventListener('click', async () => {
         console.error("Error during classification:", error);
     }
 });
+
+function showBootstrapAlert(message, type = "danger") {
+  const alertContainer = document.getElementById("alertContainer");
+  alertContainer.innerHTML = "";
+
+  const alert = document.createElement("div");
+  alert.className = `alert alert-${type} alert-dismissible fade show shadow-sm w-100`;
+  alert.role = "alert";
+  alert.innerHTML = `
+    ${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  `;
+
+  alertContainer.appendChild(alert);
+
+  setTimeout(() => {
+    alert.classList.remove("show");
+    setTimeout(() => alert.remove(), 150);
+  }, 3000);
+}
+
