32619b3736263c332601037cb4b7fddd7c1cafa6
Guilherme Luan <guilhermeluan@pm.me>
2025-10-01 10:18:10 -0300

    feat(tests): add response handling test for email classifier
    * Implement unit test for response_to_email function.
    * Mock GenerativeModel and environment variables for testing.
    * Validate expected response generation based on email classification.

--- tests/services/test_classifier_service.py
+++ tests/services/test_classifier_service.py
@@ -3,15 +3,16 @@ from unittest.mock import patch, Mock
 from src.app import app
 from src.services.classifier_service import classify_email, response_to_email, get_classify_prompt, get_response_prompt
 
+
 class TestEmailClassifier(unittest.TestCase):
 
     @patch('src.services.classifier_service.genai.GenerativeModel')
     @patch('src.services.classifier_service.os.getenv')
-    def test_classify_email_success(self, mock_getenv, MockGenerativeModel):
+    def test_classify_email_success(self, mock_getenv, mock_generative_model):
         mock_getenv.return_value = "FakeAPIKey"
         mock_model_instance = Mock()
         mock_model_instance.generate_content.return_value = Mock(text="Productive")
-        MockGenerativeModel.return_value = mock_model_instance
+        mock_generative_model.return_value = mock_model_instance
 
         test_data = {'email': 'This is a test email.'}
         result = classify_email(test_data)
@@ -19,4 +20,21 @@ class TestEmailClassifier(unittest.TestCase):
         self.assertEqual(result, {"message": "Productive"})
 
         expected_prompt = get_classify_prompt().format(email_to_classify=test_data['email'])
+        mock_model_instance.generate_content.assert_called_once_with(expected_prompt)
+
+    @patch('src.services.classifier_service.genai.GenerativeModel')
+    @patch('src.services.classifier_service.os.getenv')
+    def test_response_to_email_success(self, mock_getenv, mock_generative_model):
+        mock_getenv.return_value = "FakeAPIKey"
+        mock_model_instance = Mock()
+        mock_model_instance.generate_content.return_value = Mock(text="Suggested response.")
+        mock_generative_model.return_value = mock_model_instance
+
+        test_data = {'email': 'This is a test email.', 'classify': 'Productive'}
+        result = response_to_email(test_data)
+
+        self.assertEqual(result, {"message": "Suggested response."})
+
+        expected_prompt = get_response_prompt().format(email_to_response=test_data['email'],
+                                                       email_classification=test_data['classify'])
         mock_model_instance.generate_content.assert_called_once_with(expected_prompt)
